f565e9f0d46e1528d2da79b292cb2f71ca360dba
Bobby Yost <bobby.9202@gmail.com>
2019-06-05 10:32:01 -0400

    Add unit tests for parsing vhost configuration files

--- gopache_test.go
+++ gopache_test.go
@@ -2,6 +2,8 @@ package gopache
 
 import (
 	"os"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -63,4 +65,34 @@ func TestFind(t *testing.T) {
 	for _, m := range matches {
 		assert.Equal(t, "LoadModule", m.Name)
 	}
+}
+
+func TestFindOneVHost(t *testing.T) {
+	file, _ := os.Open("./data/vhost.conf")
+	defer file.Close()
+
+	root, _ := Parse(file)
+
+	match, err := root.FindOne("VirtualHost")
+	if err != nil {
+		panic(err)
+	}
+	tokens := strings.Split(match.Content, ":")
+	port, _ := strconv.ParseUint(tokens[1], 10, 32)
+
+	a := assert.New(t)
+	a.Equal("172.20.30.40:4000", match.Content)
+	a.Equal(uint(4000), uint(port))
+	a.Equal("172.20.30.40", tokens[0])
+
+	docRoot, err := root.FindOne("DocumentRoot")
+	if err != nil {
+		panic(err)
+	}
+	a.Equal("/www/subdomain/sub2", strings.Trim(docRoot.Content, "\""))
+	admin, err := root.FindOne("ServerAdmin")
+	if err != nil {
+		panic(err)
+	}
+	a.Equal("name@email.corporation.com", admin.Content)
 }
